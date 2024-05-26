fdc754e0b3c1f2506c87ab2e2025ec94a46ab786
avelon54 <94259143+avelon54@users.noreply.github.com>
2024-05-26 19:31:39 +0300

    Added options for counter-clockwise direction, and starting position.

--- Route Şifre Algoritması.py
+++ Route Şifre Algoritması.py
@@ -1,74 +1,162 @@
-#Route Şifre Algoritması
-def route_sifre_coz(sifreli_metin, sutun_sayisi):
-    satir_sayisi = len(sifreli_metin) // sutun_sayisi
-    if len(sifreli_metin) % sutun_sayisi != 0:
-        raise ValueError("Metin uzunluğu sütun sayısına tam bölünmüyor.")       
-    ızgara = [['' for _ in range(sutun_sayisi)] for _ in range(satir_sayisi)]
-    satir_index= 0
-    sutun_index= sutun_sayisi -1
-    increase_satir = -1
-    increase_sutun = 1
-    sifreli_index = 0
-    direction = 0
-    satir_direction=1
-    sutun_direction = 1
-    kalan_sutun_index = sutun_sayisi -1
-    kalan_satir_index = satir_sayisi -1
-    baslangıc_satir_index = 0
-    baslangıc_sutun_index= 0
+#Route Crypto Algorithm
+def route_sifre_coz(encrypted_text: str, column_count: int, clockwise = True, starting_position = 1):  
+    """
+    This function decrypts a message that has been encrypted using the route cipher algorithm, based on the specified direction (clockwise or counter-clockwise).
+
+    Parameters:
+
+        encrypted_text (str): The encrypted message to be decrypted.
+
+        column_count (int): The number of columns used in the route cipher algorithm.
+
+        clockwise (bool, optional): The direction of the decryption process. If True, the decryption is performed in a clockwise direction. If False,
+        it is performed counter-clockwise. Default is True.
+        
+        starting_position (int, optional): The starting position for the decryption process. The possible values are:
+        1: Top-right corner
+        2: Bottom-right corner
+        3: Bottom-left corner
+        4: Top-left corner
+        Default is 1.
+
+    Returns:
+    str: The decrypted message.
+
+    Raises:
+    ValueError: If the length of the encrypted text is not divisible by the column count, or if the starting position is not one of the valid options (1, 2, 3, 4).
+    """
+    row_count = len(encrypted_text) // column_count
+    if len(encrypted_text) % column_count != 0:
+        raise ValueError("The length of the encrypted text must be divisible by the column count.")
+    if starting_position not in (1, 2, 3, 4):
+        raise ValueError("The starting position must be one of the following: 1, 2, 3, 4.")
+    
+    table = [['' for _ in range(column_count)] for _ in range(row_count)]
+    if starting_position == 1:
+        row_index= 0
+        column_index= column_count -1
+        direction_r_or_c = 0
+        if clockwise:    
+            row_direction=1
+            column_direction = 1
+            increase_row = -1
+            increase_column = 1  
+        else:
+            row_direction= 0
+            column_direction = 0
+            increase_row = -1
+            increase_column = 1  
+    elif starting_position ==2:
+        row_index= row_count -1
+        column_index= column_count -1
+        direction_r_or_c = 1
+
+        if clockwise:
+            row_direction=1
+            column_direction = 0    
+            increase_row = 1
+            increase_column = 1  
+        else:
+            row_direction=0
+            column_direction = 1    
+            increase_row = 1
+            increase_column = 1
+    elif starting_position == 3:
+        row_index= row_count -1
+        column_index= 0
+        direction_r_or_c = 0
+
+        if clockwise:
+            row_direction=0
+            column_direction = 0    
+            increase_row = 1
+            increase_column = -1  
+        else:
+            row_direction= 1
+            column_direction = 1    
+            increase_row = 1
+            increase_column = -1
+    elif starting_position ==4:
+        row_index= 0
+        column_index= 0
+        direction_r_or_c = 1
+
+        if clockwise:
+            row_direction=0
+            column_direction = 1    
+            increase_row = -1
+            increase_column = -1  
+        else:
+            row_direction=1
+            column_direction = 0   
+            increase_row = -1
+            increase_column = -1
+    encrypted_text_index = 0
+    last_column_index = column_count -1
+    last_row_index = row_count -1
+    initial_row_index = 0
+    initial_column_index= 0
     rotate = True
-    while sifreli_index < satir_sayisi*sutun_sayisi:
-        ızgara[satir_index][sutun_index] = sifreli_metin[sifreli_index]
-        sifreli_index  += 1
+    while encrypted_text_index < row_count*column_count:
+        table[row_index][column_index] = encrypted_text[encrypted_text_index]
+        encrypted_text_index  += 1
+        print("******************")
+        for rows in table:           
+            print(rows)            
+        print("******************")
         if rotate:
-            if satir_direction == 0 and satir_index == baslangıc_satir_index and satir_index != kalan_satir_index:
+            if row_direction == 0 and row_index == initial_row_index and row_index != last_row_index:
                 
-                satir_direction = 1
-                baslangıc_satir_index += 1
-                direction = 1
-                increase_sutun *= -1
+                row_direction = 1
+                initial_row_index += 1
+                direction_r_or_c = 1
+                increase_column *= -1
                 
-            elif sutun_direction == 0 and sutun_index == baslangıc_sutun_index and sutun_index != kalan_sutun_index:
+            elif column_direction == 0 and column_index == initial_column_index and column_index != last_column_index:
                 
-                sutun_direction = 1
-                baslangıc_sutun_index += 1
-                direction = 0
-                increase_satir *= -1    
+                column_direction = 1
+                initial_column_index += 1
+                direction_r_or_c = 0
+                increase_row *= -1    
                 
-            elif satir_direction == 1 and satir_index == kalan_satir_index and satir_index != baslangıc_satir_index:
+            elif row_direction == 1 and row_index == last_row_index and row_index != initial_row_index:
                 
-                satir_direction = 0
-                increase_sutun *= -1
-                kalan_satir_index -= 1
-                direction = 1
+                row_direction = 0
+                increase_column *= -1
+                last_row_index -= 1
+                direction_r_or_c = 1
                 
-            elif sutun_direction == 1 and sutun_index == kalan_sutun_index and sutun_index != baslangıc_sutun_index:     
-                sutun_direction = 0
-                direction = 0
-                increase_satir *= -1
-                kalan_sutun_index -= 1   
-            if kalan_satir_index == baslangıc_satir_index == satir_index:
-                direction = 1
-                increase_sutun *= -1
+            elif column_direction == 1 and column_index == last_column_index and column_index != initial_column_index:     
+                column_direction = 0
+                direction_r_or_c = 0
+                increase_row *= -1
+                last_column_index -= 1   
+            if last_row_index == initial_row_index == row_index:
+                direction_r_or_c = 1
+                increase_column *= -1
                 rotate = False
-            if kalan_sutun_index == baslangıc_sutun_index == sutun_index:
-                direction= 0
-                increase_satir *= -1
+            if last_column_index == initial_column_index == column_index:
+                direction_r_or_c = 0
+                increase_row *= -1
                 rotate = False 
         
-        if direction == 0:
-            satir_index += increase_satir
-        if direction == 1:
-            sutun_index += increase_sutun
+        if direction_r_or_c == 0:
+            row_index += increase_row
+        if direction_r_or_c == 1:
+            column_index += increase_column
         
     cozulmus_metin = ''
-    for satir in range(satir_sayisi):
-        for sutun in range(sutun_sayisi):
-            cozulmus_metin += ızgara[satir][sutun]
+    for row in range(row_count):
+        for column in range(column_count):
+            cozulmus_metin += table[row][column]
     
     return cozulmus_metin
 
-route_sifreli_metin = 'AİRERXXEDRANNVATÇEHEYNHİ'
-sutun_sayisi = 4
-sonuc = route_sifre_coz(route_sifreli_metin, sutun_sayisi)
+# route_encrypted_text = 'XRAASELLM'
+# column_count = 3
+# sonuc = route_sifre_coz(route_encrypted_text,column_count, True , 2)
+
+route_encrypted_text = 'AİRERXXEDRANNVATÇEHEYNHİ'
+column_count = 4
+sonuc = route_sifre_coz(route_encrypted_text,column_count, True , 1)
 print(sonuc)
